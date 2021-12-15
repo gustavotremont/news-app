b328ae89821389995b6c04932d3578796b22b6a8
Gustavo <gustavotremontsr@gmail.com>
2021-12-15 15:37:38 +0100

    create routing with navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Nav from '../Nav'
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <header>
+        <Nav/>
+      </header>
     )
   }
 }
