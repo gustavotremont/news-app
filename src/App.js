1171013037cd47d9caab40532926484c2eb70e7f
Gustavo Tremont <90655835+gustavotremont@users.noreply.github.com>
2021-12-15 15:41:32 +0100

    Revert "React routing"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header/>
-        <Main/>
-      </BrowserRouter>
-      <Footer/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
