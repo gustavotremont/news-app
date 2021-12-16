b2baeeae5a4599ee88fa3411fa32413f15e710d2
Gustavo <gustavotremontsr@gmail.com>
2021-12-16 19:10:50 +0100

    create base material navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,150 @@ import React, { Component } from 'react'
 import Nav from '../Nav'
 import {userContext} from '../../context/UserContext';
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
 export default class Header extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      anchorElNav: null,
+      anchorElUser: null
+    }
+  }
+
+  handleOpenNavMenu = (event) => {
+    this.setState({anchorElNav: event.currentTarget})
+  };
+
+  handleOpenUserMenu = (event) => {
+    this.setState({anchorElUser: event.currentTarget})
+  };
+
+  handleCloseNavMenu = () => {
+    this.setState({anchorElNav: null})
+  };
+
+  handleCloseUserMenu = () => {
+    this.setState({anchorElUser: null})
+  };
+
   render() {
     return (
-      <header>
-        <userContext.Consumer>
+      <>
+
+        <AppBar position="static">
+              <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                  <Typography
+                    variant="h6"
+                    noWrap
+                    component="div"
+                    sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                  >
+                    Mi Icono
+                  </Typography>
+
+                  <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                      size="large"
+                      aria-label="account of current user"
+                      aria-controls="menu-appbar"
+                      aria-haspopup="true"
+                      onClick={this.handleOpenNavMenu}
+                      color="inherit"
+                    >
+                      <MenuIcon />
+                    </IconButton>
+                    <Menu
+                      id="menu-appbar"
+                      anchorEl={this.state.anchorElNav}
+                      anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                      }}
+                      open={Boolean(this.state.anchorElNav)}
+                      onClose={this.handleCloseNavMenu}
+                      sx={{
+                        display: { xs: 'block', md: 'none' },
+                      }}
+                    >
+                      {pages.map((page) => (
+                        <MenuItem key={page} onClick={this.handleCloseNavMenu}>
+                          <Typography textAlign="center">{page}</Typography>
+                        </MenuItem>
+                      ))}
+                    </Menu>
+                  </Box>
+
+                  <Typography
+                    variant="h6"
+                    noWrap
+                    component="div"
+                    sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                  >
+                    LOGO
+                  </Typography>
+                  
+                  <Box sx={{ flexGrow: 0 }}>
+                    <Nav/>
+                  </Box>
+
+                  <Box sx={{ flexGrow: 0 }}>
+                    <Tooltip title="Open settings">
+                      <IconButton onClick={this.handleOpenUserMenu} sx={{ p: 0 }}>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                      </IconButton>
+                    </Tooltip>
+                    <Menu
+                      sx={{ mt: '45px' }}
+                      id="menu-appbar"
+                      anchorEl={this.state.anchorElUser}
+                      anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      open={Boolean(this.state.anchorElUser)}
+                      onClose={this.handleCloseUserMenu}
+                    >
+                      {settings.map((setting) => (
+                        <MenuItem key={setting} onClick={this.handleCloseNavMenu}>
+                          <Typography textAlign="center">{setting}</Typography>
+                        </MenuItem>
+                      ))}
+                    </Menu>
+                  </Box>
+                </Toolbar>
+              </Container>
+            </AppBar>
+
+
+        {/* <userContext.Consumer>
           {
             ({user, logout}) => 
               user
@@ -18,12 +157,14 @@ export default class Header extends Component {
                 </>
               : null    
           }
-        </userContext.Consumer>
+        </userContext.Consumer> */}
+
         {/* <section>
           <img src="https://cdn-icons-png.flaticon.com/512/21/21601.png" alt="logo" />
         </section> */}
-        <Nav/>
-      </header>
+
+        {/* <Nav/> */}
+      </>
     )
   }
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,49 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+const routes = [{path: '/', name: 'Home'}, {path: '/form', name: 'Home'}, {path: '/list', name: 'Home'}];
 
 export default class Nav extends Component {
+  
   render() {
     return (
       <nav>
-          <ul>
-            <li> <Link to="/">Home</Link> </li>
-            <li> <Link to="/form">Create</Link> </li>
-            <li> <Link to="/list">Dashboard</Link> </li>
-          </ul>
+
+      <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {routes.map(route => {
+              return <Button
+                        onClick={this.handleCloseNavMenu}
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                        >
+                          <Link to={route.path}>{route.name}</Link>
+                      </Button>
+            })}
+      </Box>
+
+      
+          {/* <Button
+          onClick={this.handleCloseNavMenu}
+          sx={{ my: 2, color: 'white', display: 'block' }}
+          >
+            <Link to="/">Home</Link>
+          </Button>
+
+          <Button
+          onClick={this.handleCloseNavMenu}
+          sx={{ my: 2, color: 'white', display: 'block' }}
+          >
+            <Link to="/form">Create</Link>
+          </Button>
+
+          <Button
+          onClick={this.handleCloseNavMenu}
+          sx={{ my: 2, color: 'white', display: 'block' }}
+          >
+            <Link to="/list">Dashboard</Link>
+          </Button> */}
+          
       </nav>
     )
   }
